85fd7832533b8c44541a8fa78cb18307e1921ac7
JH <jolley003@gmail.com>
2019-04-26 21:27:29 +0200

    Fix nav, animate nav

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ $('.nav__burger').on('click', function () {
     $('.nav__icon').toggleClass('active');
     $('.nav__list').slideToggle();
 })
+
+$('.nav__link').hover(function () {
+    $(this).parent().toggleClass('active');
+})
+
+$(document).on('scroll', function () {
+    if ($(document).scrollTop() > 10) {
+        $('.nav, .nav__logo, .nav__burger').addClass('scrolled');
+    } else $('.nav, .nav__logo, .nav__burger').removeClass('scrolled');
+})
